e146dde84e3e992d35176bf20cfb01539a1ce331
vincentgrassin <vincent.grassin@troopers.email>
2022-12-05 18:38:31 +0100

    add bestof

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import { resources } from "../resources";
-	import Filters from "$lib/Filters.svelte";
+	import { dataAlbums } from "../dataPicture";
+	import type { Picture } from "src/types";
+	import PicturesGallery from "$lib/PicturesGallery.svelte";
+
+	const allPictures = Object.values(dataAlbums).reduce((acc: Picture[], album) => {
+		if (album.pictures) {
+			const bestPictures = album.pictures.filter((picture) => picture.quality === 2);
+			return [...acc, ...bestPictures];
+		}
+		return acc;
+	}, []);
 </script>
 
 <div>
-	<h1>{resources.search}</h1>
-	<Filters />
+	<h1>{resources.bestOf}</h1>
+	<PicturesGallery pictures={allPictures} />
 </div>
 
 <style></style>
